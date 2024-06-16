3e5de2881a20097014413b3b361efb81658cc01c
thanhty6701 <th.ty6701@gmail.com>
2024-06-16 15:21:12 +0700

    Congif mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import Button from '@mui/material/Button'
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
 import { pink } from '@mui/material/colors'
 import HomeIcon from '@mui/icons-material/Home'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>mike</div>
+
+      <Typography variant="body2" color="text.secondary"> Tesx</Typography>
+
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" color='success'>Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <br />
+
       <AccessAlarm></AccessAlarm>
       <ThreeDRotation></ThreeDRotation>
       <HomeIcon color="primary" />
